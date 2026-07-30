8ab7c19ec40ee38dd9aca7e71a016e5121894ea5
eddySantiagoGuarin <eddyS.guarinc@uqvirtual.edu.co>
2026-07-30 09:40:58 -0500

    feat: se crea microservicio

---
+++ src/main/java/com/world_dance/ms_notification_/_streaming/Application.java
@@ -0,0 +1,13 @@
+package com.world_dance.ms_notification_._streaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/world_dance/ms_notification_/_streaming/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.world_dance.ms_notification_._streaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
