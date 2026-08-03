7de31e0b57a87faa6c90215b6a908ccf23b1ef04
eddySantiagoGuarin <eddyS.guarinc@uqvirtual.edu.co>
2026-08-03 09:55:41 -0500

    feat: se agrego la configuracion inicial del microservicio

---
+++ src/main/java/com/world_dance/ms_notification_/_streaming/service/streamSessionService.java
@@ -0,0 +1,17 @@
+package com.world_dance.ms_notification_._streaming.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+
+
+
+@Service
+@RequiredArgsConstructor
+public class streamSessionService {
+
+    
+
+
+}
